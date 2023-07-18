0ec697427bc64a02fd3f1984abe44bfff8e70aa5
Madhav Santhosh <madhavsantos@gmail.com>
2023-07-18 23:52:42 +0530

    added signup page

--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -1,5 +1,6 @@
 import 'package:dhwani_app_miniproject/screens/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:dhwani_app_miniproject/screens/login_page.dart';
 
 class DhwaniApp_SignupPage extends StatefulWidget {
   const DhwaniApp_SignupPage({Key? key}) : super(key: key);
@@ -9,6 +10,16 @@ class DhwaniApp_SignupPage extends StatefulWidget {
 }
 
 class _DhwaniApp_SignupPageState extends State<DhwaniApp_SignupPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController gnameController = TextEditingController();
+  TextEditingController relationController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController passwordconController = TextEditingController();
+  TextEditingController disabilityController = TextEditingController();
+  TextEditingController bloodController = TextEditingController();
+  TextEditingController numberController = TextEditingController();
+  TextEditingController genderController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,17 +39,138 @@ class _DhwaniApp_SignupPageState extends State<DhwaniApp_SignupPage> {
                 style: TextStyle(fontSize: 32),
               ),
             ),
-            Text("Sign-Up Parameters"),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'User Name',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                obscureText: true,
+                controller: passwordconController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Confirm Password',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: genderController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Gender',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: bloodController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Blood Group',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: numberController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Contact Number',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: disabilityController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Disability type',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Guardian Name',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: relationController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Relation of Guardian',
+                ),
+              ),
+            ),
             Container(
               height: 50,
               padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
               child: ElevatedButton(
                 child: const Text('SignUp'),
                 onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => DhwaniApp_LoginPage()));
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => DhwaniApp_LoginPage()));
                 },
               ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('Already have an account?'),
+                TextButton(
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => DhwaniApp_LoginPage()));
+                  },
+                ),
+              ],
             ),
           ],
         ),
