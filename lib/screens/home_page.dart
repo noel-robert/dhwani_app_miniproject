bfd00806e3503d2cb254bb54595c0ffdbe442f44
Noel John Robert <noel060202@gmail.com>
2023-09-04 00:40:37 +0530

    modified home_page.dart to load data from HIVE database, not from JSON file

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,22 +1,16 @@
 import 'dart:convert';
-import 'dart:io';
-
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:line_icons/line_icons.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 import '../controllers/bottom_bar_controller.dart';
 import '../widgets/custom_card_widget.dart';
+import 'package:dhwani_app_miniproject/models/card_model.dart';
+
 
 class DhwaniApp_HomePage extends StatefulWidget {
-  final List<List<String>> selectedAnswers;
-
-  const DhwaniApp_HomePage({Key? key, required this.selectedAnswers})
-      : super(key: key);
-
   @override
   _DhwaniApp_HomePageState createState() => _DhwaniApp_HomePageState();
 }
@@ -24,100 +18,58 @@ class DhwaniApp_HomePage extends StatefulWidget {
 final BottomBarController controller = Get.put(BottomBarController());
 
 class _DhwaniApp_HomePageState extends State<DhwaniApp_HomePage> {
-  List<CardWidget> cards = [];
-  List<int> clickCounts = [];
+  late Box<CardModel> cardBox;
   bool _languageSwitchState = false; // language is malayalam | english
+  bool _isNewUser = true;
 
   @override
   void initState() {
     super.initState();
-    _loadData();
-    _loadClickCounts();
+    _openCardBox();
+    _checkIfNewUser();
+  }
+
+  Future<void> _openCardBox() async {
+    cardBox = await Hive.openBox<CardModel>('cards');
+  }
+
+  Future<void> _checkIfNewUser() async {
+    if (cardBox.isEmpty) {
+      await _loadData();
+      _isNewUser = true;
+    } else {
+      _isNewUser = false;
+    }
   }
 
   Future<void> _loadData() async {
-    // get app documents directory
-    Directory appDocumentsDirectory = await getApplicationDocumentsDirectory();
-    String cardDataPath = '${appDocumentsDirectory.path}/card_Data_updated.json';
+    final jsonString = await DefaultAssetBundle.of(context).loadString('assets/dataFiles/card_Data.json');
+    // print(jsonString);
+    final jsonData = jsonDecode(jsonString);
 
-    String jsonContent = await File(cardDataPath).readAsString();
-    print(jsonContent);
-    List<dynamic> jsonData = jsonDecode(jsonContent);
+    // Remove this line if you want to keep existing data in the box
+    cardBox.clear();
 
-    // setState(() {
-    //   cards = jsonData
-    //       .map((data) => CardWidget(
-    //             imagePath: data['imagePath'],
-    //             title: data['title'],
-    //             isFav: data['isFav'],
-    //             onUpdate: _updateClickCounts,
-    //             description: data['description'],
-    //             malluDescription: data['malluDescription'],
-    //             tags: List<String>.from(data['tags']),
-    //             languageSwitchState: _languageSwitchState,
-    //           ))
-    //       .toList();
-    // });
-    List<CardWidget> tempCards = jsonData.map((data) {
-      return CardWidget(
-        imagePath: data['imagePath'],
-        title: data['title'],
-        isFav: data['isFav'],
-        onUpdate: _updateClickCounts,
-        description: data['description'],
-        malluDescription: data['malluDescription'],
-        tags: List<String>.from(data['tags']),
-        languageSwitchState: _languageSwitchState,
+    for (final cardData in jsonData) {
+      final card = CardModel(
+        imagePath: cardData['imagePath'],
+        title: cardData['title'],
+        isFav: cardData['isFav'],
+        description: cardData['description'],
+        malluDescription: cardData['malluDescription'],
+        tags: List<String>.from(cardData['tags']),
+        clickCount: 0,
       );
-    }).toList();
-
-    setState(() {
-      cards = tempCards;
-    });
+      cardBox.add(card);
+    }
   }
 
-  Future<void> _loadClickCounts() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
+  void _incrementCounter(CardModel card) {
     setState(() {
-      clickCounts = cards.map((card) {
-        return prefs.getInt('${card.title}_clickCount') ?? 0;
-      }).toList();
+      card.clickCount++;
+      card.save();  // update database
     });
   }
-
-  void _updateClickCounts() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      clickCounts = cards.map((card) {
-        return prefs.getInt('${card.title}_clickCount') ?? 0;
-      }).toList();
-    });
-  }
-  // void _updateClickCounts() async {
-  //   SharedPreferences prefs = await SharedPreferences.getInstance();
-  //   setState(() {
-  //     for (var card in cards) {
-  //       int clickCount = prefs.getInt('${card.title}_clickCount') ?? 0;
-  //       if (card.isFav) { clickCount += 10; }
-  //       prefs.setInt('${card.title}_clickCount', clickCount);
-  //     }
-  //     clickCounts = cards.map((card) {
-  //       return prefs.getInt('${card.title}_clickCount') ?? 0;
-  //     }).toList();
-  //   });
-  // }
-  // @override
-  // void didChangeDependencies() {
-  //   super.didChangeDependencies();
-  //       _updateClickCounts();
-  // }
-  // @override
-  // void didUpdateWidget(DhwaniApp_HomePage oldWidget) {
-  //   super.didUpdateWidget(oldWidget);
-  //   if(oldWidget.selectedAnswers != widget.selectedAnswers) {
-  //     _updateClickCounts();
-  //   }
-  // }
 
   void _toggleLanguageSwitch() {
     setState(() {
@@ -127,30 +79,30 @@ class _DhwaniApp_HomePageState extends State<DhwaniApp_HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    List<int> sortedIndexes =
-        List.generate(clickCounts.length, (index) => index)
-          ..sort((a, b) => clickCounts[b].compareTo(clickCounts[a]));
-
-    List<CardWidget> sortedCards =
-        sortedIndexes.map((index) => cards[index]).toList();
-
-    // update isFav value from selectedAnswers
-    for (int i = 0; i < widget.selectedAnswers.length; i++) {
-      List<String> selectedOptions = widget.selectedAnswers[i];
-      if (selectedOptions.isNotEmpty) {
-        String selectedOption = selectedOptions.first;
-
-        for (int j = 0; j < sortedCards.length; j++) {
-          if (sortedCards[j].title == selectedOption) {
-            setState(() {
-              sortedCards[j].isFav = true;
-            });
-            break;
-          }
-        }
-      }
-    }
-
+    // List<int> sortedIndexes =
+    //     List.generate(clickCounts.length, (index) => index)
+    //       ..sort((a, b) => clickCounts[b].compareTo(clickCounts[a]));
+    //
+    // List<CardWidget> sortedCards =
+    //     sortedIndexes.map((index) => cards[index]).toList();
+    //
+    // // update isFav value from selectedAnswers
+    // for (int i = 0; i < widget.selectedAnswers.length; i++) {
+    //   List<String> selectedOptions = widget.selectedAnswers[i];
+    //   if (selectedOptions.isNotEmpty) {
+    //     String selectedOption = selectedOptions.first;
+    //
+    //     for (int j = 0; j < sortedCards.length; j++) {
+    //       if (sortedCards[j].title == selectedOption) {
+    //         setState(() {
+    //           sortedCards[j].isFav = true;
+    //         });
+    //         break;
+    //       }
+    //     }
+    //   }
+    // }
+    final List<CardModel> cards = _isNewUser ? cardBox.values.toList() : cardBox.values.toList()..sort((a, b) => b.clickCount.compareTo(a.clickCount));
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -158,62 +110,84 @@ class _DhwaniApp_HomePageState extends State<DhwaniApp_HomePage> {
         // backgroundColor: Colors.transparent,
         elevation: 8,
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                const Text(
-                  'MALAYALAM',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(width: 16.0),
-                Ink(
-                  height: 26.0,
-                  width: 40.0,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(25.0),
-                      color: _languageSwitchState ? Colors.green : Colors.red),
-                  child: InkWell(
-                    // onTap: () {
-                    //   setState(() {
-                    //     _languageSwitchState = !_languageSwitchState;
-                    //   });
-                    // },
-                    onTap: _toggleLanguageSwitch,
-                    borderRadius: BorderRadius.circular(25.0),
-                    child: Center(
-                      child: Text(
-                        _languageSwitchState ? 'ON' : 'OFF',
-                        style: const TextStyle(
-                            fontSize: 13.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      ),
-                    ),
-                  ),
-                )
-              ],
+
+      // body: Column(
+      //   children: [
+      //     Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: Row(
+      //         mainAxisAlignment: MainAxisAlignment.end,
+      //         children: [
+      //           const Text(
+      //             'MALAYALAM',
+      //             style: TextStyle(
+      //               fontSize: 16.0,
+      //               fontWeight: FontWeight.bold,
+      //             ),
+      //           ),
+      //           const SizedBox(width: 16.0),
+      //           Ink(
+      //             height: 26.0,
+      //             width: 40.0,
+      //             decoration: BoxDecoration(
+      //                 borderRadius: BorderRadius.circular(25.0),
+      //                 color: _languageSwitchState ? Colors.green : Colors.red),
+      //             child: InkWell(
+      //               // onTap: () {
+      //               //   setState(() {
+      //               //     _languageSwitchState = !_languageSwitchState;
+      //               //   });
+      //               // },
+      //               onTap: _toggleLanguageSwitch,
+      //               borderRadius: BorderRadius.circular(25.0),
+      //               child: Center(
+      //                 child: Text(
+      //                   _languageSwitchState ? 'ON' : 'OFF',
+      //                   style: const TextStyle(
+      //                       fontSize: 13.0,
+      //                       fontWeight: FontWeight.bold,
+      //                       color: Colors.white),
+      //                 ),
+      //               ),
+      //             ),
+      //           )
+      //         ],
+      //       ),
+      //     ),
+      //     Expanded(
+      //       child: GridView.builder(
+      //         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      //           crossAxisCount: 2,
+      //           mainAxisSpacing: 10,
+      //           crossAxisSpacing: 10,
+      //         ),
+      //         itemCount: sortedCards.length,
+      //         itemBuilder: (context, index) => sortedCards[index],
+      //       ),
+      //     )
+      //   ],
+      // ),
+      body: ValueListenableBuilder(
+        valueListenable: cardBox.listenable(),
+        builder: (context, box, widget) {
+          return GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
             ),
-          ),
-          Expanded(
-            child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 10,
-                crossAxisSpacing: 10,
-              ),
-              itemCount: sortedCards.length,
-              itemBuilder: (context, index) => sortedCards[index],
-            ),
-          )
-        ],
+            itemCount: cards.length,
+            itemBuilder: (context, index) {
+              final card = cards[index];
+              return CustomCardWidget(
+                card: card,
+                onTap: () => _incrementCounter(card),
+              );
+            }
+          );
+        },
       ),
+
       bottomNavigationBar: Container(
         decoration: BoxDecoration(color: Colors.white, boxShadow: [
           BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
