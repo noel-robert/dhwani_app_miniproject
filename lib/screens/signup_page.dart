4ea7e17c791a9fe02dfe3a0eeaffc4a50d26cf32
Noel John Robert <noel060202@gmail.com>
2023-09-03 18:14:35 +0530

    modified signup_page.dart to store data in HIVE database. also added visibility toggles

--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -1,6 +1,12 @@
+import 'package:dhwani_app_miniproject/models/userData_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+
 import 'package:dhwani_app_miniproject/screens/login_page.dart';
 import 'package:dhwani_app_miniproject/screens/questionnaire_page.dart';
-import 'package:flutter/material.dart';
+
 
 class DhwaniApp_SignupPage extends StatefulWidget {
   const DhwaniApp_SignupPage({Key? key}) : super(key: key);
@@ -10,16 +16,97 @@ class DhwaniApp_SignupPage extends StatefulWidget {
 }
 
 class _DhwaniApp_SignupPageState extends State<DhwaniApp_SignupPage> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController gnameController = TextEditingController();
-  TextEditingController relationController = TextEditingController();
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController guardiannameController = TextEditingController();
+  TextEditingController guardianrelationController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-  TextEditingController passwordconController = TextEditingController();
-  TextEditingController disabilityController = TextEditingController();
-  TextEditingController bloodController = TextEditingController();
-  TextEditingController numberController = TextEditingController();
+  TextEditingController passwordconfirmationController = TextEditingController();
+  TextEditingController disabilitytypeController = TextEditingController();
+  TextEditingController bloodgroupController = TextEditingController();
+  TextEditingController contactnumberController = TextEditingController();
   TextEditingController genderController = TextEditingController();
+
+  String? selectedGender = '-select-';
+  String? selectedBloodGroup = '-select-';
+
+  bool isPasswordVisible = false;
+  bool isConfirmPasswordVisible = false;
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    Hive.close(); // close HIVE box
+    super.dispose();
+  }
+
+  Future<void> _initializeHive() async {
+    final appDocumentDirectory = await path_provider.getApplicationDocumentsDirectory();
+    Hive.init(appDocumentDirectory.path);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeHive();
+  }
+
+  Future<void> _handleSignUp() async {
+    if(_formKey.currentState!.validate()) {
+      final username = usernameController.text;
+      final password = passwordController.text;
+      final email = emailController.text;
+      final gender = selectedGender;
+      final bloodGroup = selectedBloodGroup;
+      final contactNumber = contactnumberController.text;
+      final disabilityType = disabilitytypeController.text;
+      final guardianName = guardiannameController.text;
+      final guardianRelation = guardianrelationController.text;
+
+      final userData = UserData(
+        username: username,
+        password: password,
+        email: email,
+        gender: gender,
+        bloodGroup: bloodGroup,
+        contactNumber: contactNumber,
+        disabilityType: disabilityType,
+        guardianName: guardianName,
+        guardianRelation: guardianRelation,
+      );
+
+      // check is a user with same username already exists
+      final userBox = await Hive.openBox('users');
+      if (userBox.containsKey(username)) {
+        // username already exists
+        BuildContext currentContext = context;
+        showDialog(
+          context: currentContext,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text('Signup Failed'),
+              content: const Text('Username already exists'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(currentContext).pop(); // Dismiss the dialog box
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        userBox.put(username, userData.toMap()); // add user to database
+
+        // goto questionnaire page
+        Navigator.push(context, MaterialPageRoute(builder: (context) => DhwaniApp_QuestionnairePage()));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,150 +116,226 @@ class _DhwaniApp_SignupPageState extends State<DhwaniApp_SignupPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(10),
-              child: const Text(
-                'SignUp',
-                style: TextStyle(fontSize: 32),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const Text('SignUp', style: TextStyle(fontSize: 32)),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'User Name',
+
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: usernameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'UserName',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) return 'Please enter username';
+                    return null;
+                  },
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
+
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                  ),
+                  validator: (value) {
+                    // validate email
+                    return null;
+                  },
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
+
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextFormField(
+                  obscureText: !isPasswordVisible,
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    labelText: 'Password',
+                    suffixIcon: IconButton(
+                      icon: Icon(isPasswordVisible ? Icons.visibility : Icons.visibility_off),
+                      onPressed: () {
+                        setState(() {
+                          isPasswordVisible = !isPasswordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) return 'Please enter password';
+                    return null;
+                  },
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                obscureText: true,
-                controller: passwordconController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Confirm Password',
+
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextFormField(
+                  obscureText: !isConfirmPasswordVisible,
+                  controller: passwordconfirmationController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Confirm Password',
+                    suffixIcon: IconButton(
+                      icon: Icon(isConfirmPasswordVisible ? Icons.visibility : Icons.visibility_off),
+                      onPressed: () {
+                        setState(() {
+                          isConfirmPasswordVisible = !isConfirmPasswordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty || value != passwordController.text) return 'Passwords do not match';
+                    return null;
+                  },
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: genderController,
+
+              DropdownButtonFormField<String>(
+                value: selectedGender,
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Gender',
                 ),
+                items: <String>['-select-', 'Male', 'Female', 'Other']
+                    .map<DropdownMenuItem<String>>(
+                        (String value) => DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        )
+                    ).toList(),
+                onChanged: (String? value) {
+                  setState(() {
+                    selectedGender = value;
+                  });
+                },
+                validator: (String? value) {
+                  if (value == null || value.isEmpty || value == '-select-') return 'Please select a gender';
+                  return null;
+                },
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: bloodController,
+
+              DropdownButtonFormField<String>(
+                value: selectedBloodGroup,
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Blood Group',
                 ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: numberController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Contact Number',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: disabilityController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Disability type',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: gnameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Guardian Name',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: relationController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Relation of Guardian',
-                ),
-              ),
-            ),
-            Container(
-              height: 50,
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: ElevatedButton(
-                child: const Text('SignUp'),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => DhwaniApp_QuestionnairePage()));
+                items: <String>['-select-', 'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']
+                    .map<DropdownMenuItem<String>>(
+                        (String value) => DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        )
+                    ).toList(),
+                onChanged: (String? value) {
+                  setState(() {
+                    selectedBloodGroup = value;
+                  });
+                },
+                validator: (String? value) {
+                  if (value == null || value.isEmpty || value == '-select-') return 'Please select a blood group';
+                  return null;
                 },
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text('Already have an account?'),
-                TextButton(
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(fontSize: 20),
+
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: contactnumberController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Contact Number',
                   ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => DhwaniApp_LoginPage()));
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly],
+                  validator: (value) {
+                    if (value == null || value.isEmpty) return 'Please enter a contact number';
+                    return null;
                   },
                 ),
-              ],
-            ),
-          ],
+              ),
+
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: disabilitytypeController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Disability type',
+                  ),
+                  validator: (value) {
+                    // disability type validator
+                    return null;
+                  },
+                ),
+              ),
+
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: guardiannameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Guardian Name',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) return 'Please enter name of guardian';
+                    return null;
+                  },
+                ),
+              ),
+
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: guardianrelationController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'How is Guardian related to the user?',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) return 'Please enter relation of guardian';
+                    return null;
+                  },
+                ),
+              ),
+
+              Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: ElevatedButton(
+                  onPressed: _handleSignUp, // store data onto the HIVE database
+                  child: const Text('SignUp'),
+                ),
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text('Already have an account?'),
+                  TextButton(
+                      child: const Text('Login', style: TextStyle(fontSize: 20),),
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => DhwaniApp_LoginPage()));
+                      })
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
