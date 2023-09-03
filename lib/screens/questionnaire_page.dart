a08880a11df31b6ba941fbac22c2106037de795d
Noel John Robert <noel060202@gmail.com>
2023-09-03 20:20:24 +0530

    modified questionnaire_page.dart to load questions from HIVE and also store selected answers there.

--- lib/screens/questionnaire_page.dart
+++ lib/screens/questionnaire_page.dart
@@ -1,19 +1,17 @@
 import 'dart:convert';
-import 'dart:io';
-import 'package:flutter/services.dart' show ByteData, rootBundle;
-import 'package:path_provider/path_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 import 'package:dhwani_app_miniproject/screens/home_page.dart';
-import 'package:flutter/material.dart';
-
 import '../widgets/question_widget.dart';
+
 
 class DhwaniApp_QuestionnairePage extends StatefulWidget {
   const DhwaniApp_QuestionnairePage({Key? key}) : super(key: key);
 
   @override
-  _DhwaniApp_QuestionnairePageState createState() =>
-      _DhwaniApp_QuestionnairePageState();
+  _DhwaniApp_QuestionnairePageState createState() => _DhwaniApp_QuestionnairePageState();
 }
 
 class _DhwaniApp_QuestionnairePageState extends State<DhwaniApp_QuestionnairePage> {
@@ -27,80 +25,30 @@ class _DhwaniApp_QuestionnairePageState extends State<DhwaniApp_QuestionnairePag
   }
 
   Future<void> _loadQuestions() async {
-    String jsonContent = await DefaultAssetBundle.of(context).loadString('assets/dataFiles/questionnaire_Data.json');
-    List<dynamic> jsonData = jsonDecode(jsonContent);
+    final jsonString = await rootBundle.loadString('assets/dataFiles/questionnaire_Data.json');
+    final jsonData = jsonDecode(jsonString);
+
+    final questionBox = await Hive.openBox<Question>('questions');
+    questionBox.clear();  // clear all data in questionBox
+
+    for (final questionData in jsonData) {
+      final question = Question(questionData['questionText'], List<String>.from(questionData['options']),);
+      questionBox.add(question);
+    }
 
     setState(() {
-      questions = jsonData.map((data) => Question(
-        data['questionText'],
-        List<String>.from(data['options']),
-      )).toList();
+      questions = questionBox.values.toList();
       selectedAnswers = List<List<String>>.filled(questions.length, []);
     });
   }
 
-  // void _updateCardData() async {
-  //   // documents directory path
-  //   Directory appDocumentsDirectory = await getApplicationDocumentsDirectory();
-  //
-  //   // paths for files
-  //   String cardDataPath = 'assets/dataFiles/card_Data.json';
-  //   String updatedCardDataPath = '$appDocumentsDirectory/card_Data_updated.json';
-  //
-  //   // copy contents from assets to app's document directory
-  //   ByteData fileData = await rootBundle.load(cardDataPath);
-  //   List<int> bytes = fileData.buffer.asUint8List(fileData.offsetInBytes, fileData.lengthInBytes);
-  //   await File(updatedCardDataPath).writeAsBytes(bytes);
-  //
-  //   // read contents of card_Data.json
-  //   String jsonContent = await File(updatedCardDataPath).readAsString();
-  //   List<dynamic> jsonData = jsonDecode(jsonContent);
-  //
-  //   // update isFav value in new file
-  //   for (int i=0; i<jsonData.length; i++) {
-  //     Map<String, dynamic> cardData = jsonData[i];
-  //     String cardTitle = cardData['title'];
-  //     bool isFav =  selectedAnswers.any((answers) => answers.contains(cardTitle));
-  //     cardData['isFav'] = isFav;
-  //   }
-  //
-  //   // create new copy of file with updated data from questionnaire
-  //   String updatedJsonContent = jsonEncode(jsonData);
-  //   print(updatedJsonContent);
-  //   await File(updatedCardDataPath).writeAsString(updatedJsonContent, flush: true);
-  // }
   void _updateCardData() async {
-    // Get the path to the app's document directory
-    Directory documentsDirectory = await getApplicationDocumentsDirectory();
-    String cardDataPath = '${documentsDirectory.path}/card_Data.json';
-    String updatedCardDataPath = '${documentsDirectory.path}/card_Data_updated.json';
+    final answersBox = await Hive.openBox<List<String>>('selected_answers');
+    answersBox.clear();  // clear all data in answersBox
 
-    // Check if the card_Data.json file already exists in the document directory
-    bool cardDataExists = await File(cardDataPath).exists();
-    if (!cardDataExists) {
-      // If the file doesn't exist, copy it from the assets folder to the document directory
-      ByteData cardDataAsset = await rootBundle.load('assets/dataFiles/card_Data.json');
-      List<int> bytes = cardDataAsset.buffer.asUint8List();
-      await File(cardDataPath).writeAsBytes(bytes);
+    for (final selectedAnswer in selectedAnswers) {
+      answersBox.add(selectedAnswer);
     }
-
-    // Read the content of card_Data.json
-    File cardDataFile = File(cardDataPath);
-    String jsonContent = await cardDataFile.readAsString();
-    List<dynamic> jsonData = jsonDecode(jsonContent);
-
-    // Update the isFav value in jsonData based on selectedAnswers
-    for (int i = 0; i < jsonData.length; i++) {
-      Map<String, dynamic> cardData = jsonData[i];
-      String cardTitle = cardData['title'];
-      bool isFav = selectedAnswers.any((answers) => answers.contains(cardTitle));
-      cardData['isFav'] = isFav;
-    }
-
-    // Create a new copy of card_Data.json with updated values
-    String updatedJsonContent = jsonEncode(jsonData);
-    // print(updatedJsonContent);
-    await File(updatedCardDataPath).writeAsString(updatedJsonContent, flush: true);
   }
 
   @override
@@ -109,17 +57,16 @@ class _DhwaniApp_QuestionnairePageState extends State<DhwaniApp_QuestionnairePag
       backgroundColor: Colors.white,
       appBar: AppBar(title: const Text("Questionnaire")),
       body: ListView.builder(
-          itemCount: questions.length,
-          itemBuilder: (BuildContext context, int index) {
-            return QuestionWidget(
-              question: questions[index],
-              onAnswerSelected: (List<String> selectedOptions) {
-                setState(() {
-                  selectedAnswers[index] = selectedOptions;
-                });
-              },
-            );
-          }),
+        itemCount: questions.length,
+        itemBuilder: (BuildContext context, int index) {
+          return QuestionWidget(
+            question: questions[index],
+            onAnswerSelected: (List<String> selectedOptions) {
+              selectedAnswers[index] = selectedOptions;
+            },
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // on submitting the questionnaire
@@ -130,7 +77,7 @@ class _DhwaniApp_QuestionnairePageState extends State<DhwaniApp_QuestionnairePag
           Navigator.push(context,
               MaterialPageRoute(builder: (context) => DhwaniApp_HomePage(selectedAnswers: selectedAnswers)));
         },
-        child: Icon(Icons.check),
+        child: const Icon(Icons.check),
       ),
     );
   }
