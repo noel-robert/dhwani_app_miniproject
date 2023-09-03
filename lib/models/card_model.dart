45139640a9a7cbec5b8f4e7ee2d2fe84d6fcea02
Noel John Robert <noel060202@gmail.com>
2023-09-04 00:37:12 +0530

    added card_model.dart to store card in HIVE database

---
+++ lib/models/card_model.dart
@@ -0,0 +1,37 @@
+import 'package:hive/hive.dart';
+
+part 'card_model.g.dart';
+
+@HiveType(typeId: 0)
+class CardModel extends HiveObject {
+  @HiveField(0)
+  late String imagePath;
+
+  @HiveField(1)
+  late String title;
+
+  @HiveField(2)
+  late bool isFav;
+
+  @HiveField(3)
+  late String description;
+
+  @HiveField(4)
+  late String malluDescription;
+
+  @HiveField(5)
+  late List<String> tags;
+
+  @HiveField(6)
+  late int clickCount;
+
+  CardModel({
+    required this.imagePath,
+    required this.title,
+    required this.isFav,
+    required this.description,
+    required this.malluDescription,
+    required this.tags,
+    required this.clickCount,
+  });
+}
