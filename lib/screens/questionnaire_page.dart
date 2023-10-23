24ce019d909b5c34fa4d4ee0aab4d6ecf65aad62
Noel John Robert <noel060202@gmail.com>
2023-10-23 15:30:45 +0530

    modified method of storing data to HIVE

--- lib/screens/questionnaire_page.dart
+++ lib/screens/questionnaire_page.dart
@@ -1,14 +1,16 @@
 import 'dart:convert';
-import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:flutter/services.dart' show rootBundle;
 
+import 'package:dhwani_app_miniproject/models/question_model.dart';
 import 'package:dhwani_app_miniproject/screens/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:hive_flutter/hive_flutter.dart';
+
 import '../widgets/question_widget.dart';
 
 
 class DhwaniApp_QuestionnairePage extends StatefulWidget {
-  const DhwaniApp_QuestionnairePage({Key? key}) : super(key: key);
+  const DhwaniApp_QuestionnairePage({super.key});
 
   @override
   _DhwaniApp_QuestionnairePageState createState() => _DhwaniApp_QuestionnairePageState();
@@ -18,35 +20,71 @@ class _DhwaniApp_QuestionnairePageState extends State<DhwaniApp_QuestionnairePag
   List<Question> questions = [];
   List<List<String>> selectedAnswers = [];
 
+  late Box questionBox;
+  late Box answersBox;
+
   @override
   void initState() {
     super.initState();
     _loadQuestions();
   }
 
+  @override
+  void dispose() {
+    questionBox.close();
+    super.dispose();
+  }
+
   Future<void> _loadQuestions() async {
     final jsonString = await rootBundle.loadString('assets/dataFiles/questionnaire_Data.json');
     final jsonData = jsonDecode(jsonString);
+    // print((jsonData[0])['questionText']);
 
-    final questionBox = await Hive.openBox<Question>('questions');
+    // final questionBox = await Hive.openBox<Question>('questions');
+    questionBox = await Hive.openBox('questions');
     questionBox.clear();  // clear all data in questionBox
 
     for (final questionData in jsonData) {
-      final question = Question(questionData['questionText'], List<String>.from(questionData['options']),);
-      questionBox.add(question);
+      final questionModelTyped = QuestionModel(
+          questionText: questionData['questionText'],
+          options: List<String>.from(questionData['options'])
+      );
+
+      final question = Question(
+        questionModelTyped.questionText,
+        questionModelTyped.options,
+      );
+      // print(questionData['questionText']);
+      // print(List<String>.from(questionData['options']));
+
+      questionBox.add(questionModelTyped);  // added using index as key - automatically
+      // print(question.questionText);
+      // print(question.options);
     }
 
+    // setState(() {
+    //   questions = questionBox.values.toList();
+    //   selectedAnswers = List<List<String>>.filled(questions.length, []);
+    // });
+    // print((questions[0]).questionText);
     setState(() {
-      questions = questionBox.values.toList();
+      questions = questionBox.values.map((dynamic questionModelTyped) {
+        return Question(
+          questionModelTyped.questionText,
+          questionModelTyped.options,
+        );
+      }).toList();
       selectedAnswers = List<List<String>>.filled(questions.length, []);
     });
   }
 
-  void _updateCardData() async {
-    final answersBox = await Hive.openBox<List<String>>('selected_answers');
+  void _updateSelectedAnswers() async {
+    answersBox = await Hive.openBox('selected_answers');
     answersBox.clear();  // clear all data in answersBox
 
-    for (final selectedAnswer in selectedAnswers) {
+    List<String> selectedAnswer;
+    for (selectedAnswer in selectedAnswers) {
+      print(selectedAnswer);
       answersBox.add(selectedAnswer);
     }
   }
@@ -60,6 +98,7 @@ class _DhwaniApp_QuestionnairePageState extends State<DhwaniApp_QuestionnairePag
         itemCount: questions.length,
         itemBuilder: (BuildContext context, int index) {
           return QuestionWidget(
+            // modifications needed here
             question: questions[index],
             onAnswerSelected: (List<String> selectedOptions) {
               selectedAnswers[index] = selectedOptions;
@@ -70,12 +109,11 @@ class _DhwaniApp_QuestionnairePageState extends State<DhwaniApp_QuestionnairePag
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // on submitting the questionnaire
-          print(selectedAnswers);
-          _updateCardData();
+          // print(selectedAnswers);
+          _updateSelectedAnswers();
 
           // redirect to Home_Page
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => DhwaniApp_HomePage()));
+          Navigator.push(context, MaterialPageRoute(builder: (context) => DhwaniApp_HomePage()));
         },
         child: const Icon(Icons.check),
       ),
