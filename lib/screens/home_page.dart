f5b7bb1d5d8994f96f8bcc5d20f91ec84fcfc024
Noel John Robert <noel060202@gmail.com>
2023-10-27 20:36:57 +0530

    added language toggle, removed unnecessary comments, and applied proper formatting

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,186 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:get/get.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:line_icons/line_icons.dart';
-import 'package:flutter_tts/flutter_tts.dart';
 
 import '../controllers/bottom_bar_controller.dart';
 import '../models/card_model.dart';
 import '../widgets/custom_card_widget.dart';
 
-// class DhwaniApp_HomePage extends StatefulWidget {
-//   @override
-//   _DhwaniApp_HomePageState createState() => _DhwaniApp_HomePageState();
-// }
-//
-// final BottomBarController controller = Get.put(BottomBarController());
-//
-// class _DhwaniApp_HomePageState extends State<DhwaniApp_HomePage> {
-//   late Box<CardModel> cardBox;
-//   bool _languageSwitchState = false; // language is malayalam | english
-//   bool _isNewUser = true;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _openCardBox();
-//
-//   }
-//
-//   Future<void> _openCardBox() async {
-//     cardBox = await Hive.openBox<CardModel>('cards');
-//     _checkIfNewUser();
-//   }
-//
-//   Future<void> _checkIfNewUser() async {
-//     if (cardBox.isEmpty) {
-//       await _loadData();
-//       _isNewUser = true;
-//     } else {
-//       _isNewUser = false;
-//     }
-//   }
-//
-//   Future<void> _loadData() async {
-//     final jsonString = await DefaultAssetBundle.of(context).loadString('assets/dataFiles/card_Data.json');
-//     // print(jsonString);
-//     final jsonData = jsonDecode(jsonString);
-//
-//     // Remove this line if you want to keep existing data in the box
-//     cardBox.clear();
-//
-//     for (final cardData in jsonData) {
-//       final card = CardModel(
-//         imagePath: cardData['imagePath'],
-//         title: cardData['title'],
-//         isFav: cardData['isFav'],
-//         description: cardData['description'],
-//         malluDescription: cardData['malluDescription'],
-//         tags: List<String>.from(cardData['tags']),
-//         clickCount: 0,
-//       );
-//       cardBox.add(card);
-//     }
-//   }
-//
-//   // void _incrementCounter(CardModel card) {
-//   //   setState(() {
-//   //     card.clickCount++;
-//   //     card.save();  // update database
-//   //   });
-//   // }
-//   void _incrementCounter(CardModel card) {
-//     final cardIndex = cardBox.values.toList().indexWhere((c) => c == card);
-//
-//     if (cardIndex != -1) {
-//       final updatedCard = cardBox.getAt(cardIndex);
-//       updatedCard?.clickCount++;
-//       // updatedCard?.save(); // temporary comment /*TODO*/
-//     }
-//     //setState(() {});
-//   }
-//
-//   void _toggleLanguageSwitch() {
-//     setState(() {
-//       _languageSwitchState = !_languageSwitchState;
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return FutureBuilder<void>(
-//         future: _openCardBox(),
-//         builder: (context, snapshot) {
-//           if (snapshot.connectionState == ConnectionState.done) {
-//             final List<CardModel> cards = _isNewUser ? cardBox.values.toList() : cardBox.values.toList()..sort((a, b) => b.clickCount.compareTo(a.clickCount));
-//             return Scaffold(
-//               backgroundColor: Colors.white,
-//               appBar: AppBar(
-//                 title: const Text('DhwaniApp HomePage'),
-//                 // backgroundColor: Colors.transparent,
-//                 elevation: 8,
-//               ),
-//               body: ValueListenableBuilder(
-//                 valueListenable: cardBox.listenable(),
-//                 builder: (context, box, widget) {
-//                   return GridView.builder(
-//                     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-//                       crossAxisCount: 2,
-//                       crossAxisSpacing: 10,
-//                       mainAxisSpacing: 10,
-//                     ),
-//                     itemCount: cards.length,
-//                     itemBuilder: (context, index) {
-//                       final card = cards[index];
-//                       return CustomCardWidget(
-//                         card: card,
-//                         onTap: () => _incrementCounter(card),
-//                       );
-//                     }
-//                   );
-//                 },
-//               ),
-//               bottomNavigationBar: Container(
-//                 decoration: BoxDecoration(color: Colors.white, boxShadow: [
-//                   BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
-//                 ]),
-//                 child: SafeArea(
-//                   child: Padding(
-//                     padding:
-//                         const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-//                     child: GNav(
-//                       rippleColor: Colors.grey[300]!,
-//                       hoverColor: Colors.grey[100]!,
-//                       gap: 8,
-//                       activeColor: Colors.black,
-//                       iconSize: 24,
-//                       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-//                       duration: const Duration(milliseconds: 400),
-//                       tabBackgroundColor: Colors.grey[100]!,
-//                       color: Colors.black,
-//                       tabs: [
-//                         GButton(
-//                           icon: LineIcons.home,
-//                           text: 'Home',
-//                           onPressed: () {},
-//                         ),
-//                         GButton(
-//                           icon: LineIcons.book,
-//                           text: 'Libraries',
-//                           onPressed: () {},
-//                         ),
-//                         GButton(
-//                           icon: LineIcons.search,
-//                           text: 'Search',
-//                           onPressed: () {},
-//                         ),
-//                         GButton(
-//                           icon: LineIcons.microphone,
-//                           text: 'Speak',
-//                           onPressed: () {},
-//                         ),
-//                       ],
-//                       selectedIndex: controller.selectedIndex,
-//                       onTabChange: (index) {
-//                         setState(() {
-//                           controller.updateIndex(index);
-//                         });
-//                       },
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             );
-//           } else {
-//             return const Scaffold(
-//               body: Center(child: CircularProgressIndicator(),),
-//             );
-//           }
-//         }
-//     );
-//   }
-// }
 
 class DhwaniApp_HomePage extends StatefulWidget {
   @override
@@ -252,27 +80,30 @@ class _DhwaniApp_HomePageState extends State<DhwaniApp_HomePage> {
   }
 
   FlutterTts flutterTts = FlutterTts();
+
   Future<void> _speakDescription(CardModel card) async {
     // String text = widget.languageSwitchState ? widget.malluDescription : widget.description;
-    // String language = widget.languageSwitchState ? 'ml-IN' : 'en-US';
-    String language = 'en-US';
+    String language = _languageSwitchState ? 'ml-IN' : 'en-US';
+    // String language = 'en-US';
     print(language); // debug
     final cardIndex = cardBox.values.toList().indexWhere((c) => c == card);
 
     if (cardIndex >= 0) {
       var selectedCard = cardBox.getAt(cardIndex) as CardModel;
-      String text = selectedCard.description;
+      String text = _languageSwitchState
+          ? selectedCard.malluDescription
+          : selectedCard.description;
       await flutterTts.setLanguage(language);
       await flutterTts.setPitch(1.0);
       await flutterTts.speak(text);
     }
   }
 
-  // void _toggleLanguageSwitch() {
-  //   setState(() {
-  //     _languageSwitchState = !_languageSwitchState;
-  //   });
-  // }
+  void _toggleVocalLanguage() {
+    setState(() {
+      _languageSwitchState = !_languageSwitchState;
+    });
+  }
 
   Widget _buildCardList() {
     // debug
@@ -293,27 +124,37 @@ class _DhwaniApp_HomePageState extends State<DhwaniApp_HomePage> {
     if (cardBox != null) {
       List<CardModel> cards = cardBox.values.toList()
         ..sort((a, b) => b.clickCount.compareTo(a.clickCount));
-      return ValueListenableBuilder(
-        valueListenable: cardBox.listenable(),
-        builder: (context, box, widget) {
-          return GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-              ),
-              itemCount: cards.length,
-              itemBuilder: (context, index) {
-                final card = cards[index];
-                return CustomCardWidget(
-                    card: card,
-                    onTap: () => {
-                          _speakDescription(card),
-                          _incrementCounter(card),
-                        });
-              });
-        },
-      );
+      return Column(children: [
+        Switch(
+          value: _languageSwitchState,
+          onChanged: (value) {
+            _toggleVocalLanguage();
+          },
+        ),
+        Expanded(
+          child: ValueListenableBuilder(
+            valueListenable: cardBox.listenable(),
+            builder: (context, box, widget) {
+              return GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                  itemCount: cards.length,
+                  itemBuilder: (context, index) {
+                    final card = cards[index];
+                    return CustomCardWidget(
+                        card: card,
+                        onTap: () => {
+                              _speakDescription(card),
+                              _incrementCounter(card),
+                            });
+                  });
+            },
+          ),
+        )
+      ]);
     } else {
       return const Center(
         child: CircularProgressIndicator(),
@@ -330,42 +171,6 @@ class _DhwaniApp_HomePageState extends State<DhwaniApp_HomePage> {
         // backgroundColor: Colors.transparent,
         elevation: 8,
       ),
-      // body: FutureBuilder<void>(
-      //   future: _openCardBox(),
-      //   builder: (context, snapshot) {
-      //     if (snapshot.connectionState == ConnectionState.done) {
-      //       // final List<CardModel> cards = _isNewUser
-      //       //     ? cardBox.values.toList()
-      //       //     : cardBox.values.toList()
-      //       //   ..sort((a, b) => b.clickCount.compareTo(a.clickCount));
-      //       final List<CardModel> cards = cardBox.values.toList()..sort((a, b) => b.clickCount.compareTo(a.clickCount));
-      //       return ValueListenableBuilder(
-      //         valueListenable: cardBox.listenable(),
-      //         builder: (context, box, widget) {
-      //           return GridView.builder(
-      //             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-      //               crossAxisCount: 2,
-      //               crossAxisSpacing: 10,
-      //               mainAxisSpacing: 10,
-      //             ),
-      //             itemCount: cards.length,
-      //             itemBuilder: (context, index) {
-      //               final card = cards[index];
-      //               return CustomCardWidget(
-      //                 card: card,
-      //                 onTap: () => _incrementCounter(card),
-      //               );
-      //             },
-      //           );
-      //         },
-      //       );
-      //     } else {
-      //       return const Center(
-      //         child: CircularProgressIndicator(),
-      //       );
-      //     }
-      //   },
-      // ),
       body: _buildCardList(),
       bottomNavigationBar: Container(
         decoration: BoxDecoration(color: Colors.white, boxShadow: [
