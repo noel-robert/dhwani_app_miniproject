2ea7c4b52c965a93fc7b707ab31f02248b6eb22d
Noel John Robert <noel060202@gmail.com>
2024-03-18 20:48:40 +0530

    "patched" errors

--- lib/screens/questionnaire_page.dart
+++ lib/screens/questionnaire_page.dart
@@ -17,8 +17,8 @@ class DhwaniApp_QuestionnairePage extends StatefulWidget {
 
 class DhwaniApp_QuestionnairePageState extends State<DhwaniApp_QuestionnairePage> {
   List<Question> questions = [];
-  List<List<String>> selectedAnswers = [];
-  Map<String, String> selectedAnswersMap = {};
+  List<List<String>> selectedFavourites = [];
+  Map<String, String> selectedFavouritesMap = {};
 
   late Box<QuestionModel> questionBox;
   late Box answersBox;
@@ -35,48 +35,49 @@ class DhwaniApp_QuestionnairePageState extends State<DhwaniApp_QuestionnairePage
     super.dispose();
   }
 
-  Future<void> _loadCardDataToHiveAndUpdate() async {
-    final jsonString = await DefaultAssetBundle.of(context).loadString('assets/dataFiles/card_Data.json');
-    final jsonData = jsonDecode(jsonString);
-    // print(jsonData);
-
-    cardBox = Hive.box('cards_HiveBox');
-    cardBox.clear();
-
-    // load card details to database - debug
-    for (final cardData in jsonData) {
-      // instead of below two lines, do this - if cardTitle in List<List<String>> selectedAnswers, then set a boolean variable to true
-      final cardTitle = cardData['title'];
-      final isFav = selectedAnswers.any((answer) => answer.contains(cardTitle)) ? true : false;
-
-      final card = CardModel(
-        imagePath: cardData['imagePath'],
-        title: cardTitle,
-        isFav: isFav,
-        description: cardData['description'],
-        malluDescription: cardData['malluDescription'],
-        tags: List<String>.from(cardData['tags']),
-        clickCount: isFav ? 5 : 0,
-        emotion: ["null"],
-      );
-      cardBox.add(card);
-    }
-
-    // debugging
-    // if (cardBox.isEmpty) { print('The box is empty'); }
-    // for (var key in cardBox.keys) {
-    //   var card = cardBox.get(key) as CardModel;
-    //   print('Card $key:');
-    //   print('Image Path: ${card.imagePath}');
-    //   print('Title: ${card.title}');
-    //   print('Is Favorite: ${card.isFav}');
-    //   print('Description: ${card.description}');
-    //   print('Mallu Description: ${card.malluDescription}');
-    //   print('Tags: ${card.tags}');
-    //   print('Click Count: ${card.clickCount}');
-    //   print('-------------------');
-    // }
-  }
+  // Future<void> _loadCardDataToHiveAndUpdate() async {
+  //   final jsonString = await DefaultAssetBundle.of(context).loadString('assets/dataFiles/card_Data.json');
+  //   final jsonData = jsonDecode(jsonString);
+  //   // print(jsonData);
+  //
+  //   cardBox = Hive.box('cards_HiveBox');
+  //   cardBox.clear();
+  //
+  //   // load card details to database - debug
+  //   for (final cardData in jsonData) {
+  //     // instead of below two lines, do this - if cardTitle in List<List<String>> selectedFavourites, then set a boolean variable to true
+  //     final cardTitle = cardData['title'];
+  //     final isFav = selectedFavourites.any((answer) => answer.contains(cardTitle)) ? true : false;
+  //
+  //     final card = CardModel(
+  //       imagePath: cardData['imagePath'],
+  //       title: cardTitle,
+  //       isFav: isFav,
+  //       description: cardData['description'],
+  //       malluDescription: cardData['malluDescription'],
+  //       tags: List<String>.from(cardData['tags']),
+  //       clickCount: isFav ? 5 : 0,
+  //       emotion: ["null"],
+  //     );
+  //     cardBox.add(card);
+  //   }
+  //
+  //   // debugging
+  //   // if (cardBox.isEmpty) { print('The box is empty'); }
+  //   // for (var key in cardBox.keys) {
+  //   //   var card = cardBox.get(key) as CardModel;
+  //   //   print('Card $key:');
+  //   //   print('Image Path: ${card.imagePath}');
+  //   //   print('Title: ${card.title}');
+  //   //   print('Is Favorite: ${card.isFav}');
+  //   //   print('Description: ${card.description}');
+  //   //   print('Mallu Description: ${card.malluDescription}');
+  //   //   print('Tags: ${card.tags}');
+  //   //   print('Click Count: ${card.clickCount}');
+  //   //   print('-------------------');
+  //   // }
+  //   print("Length of cardbox: ${cardBox.length}");
+  // }
 
   Future<void> _loadQuestions() async {
     final jsonString = await rootBundle.loadString('assets/dataFiles/questionnaire_Data.json');
@@ -90,7 +91,8 @@ class DhwaniApp_QuestionnairePageState extends State<DhwaniApp_QuestionnairePage
     for (final questionData in jsonData) {
       final questionModelTyped = QuestionModel(
           questionText: questionData['questionText'],
-          options: List<String>.from(questionData['options']));
+          options: List<String>.from(questionData['options'])
+      );
 
       // final question = Question(
       //   questionModelTyped.questionText,
@@ -107,7 +109,7 @@ class DhwaniApp_QuestionnairePageState extends State<DhwaniApp_QuestionnairePage
           questionModelTyped.options,
         );
       }).toList();
-      selectedAnswers = List<List<String>>.filled(questions.length, []);
+      selectedFavourites = List<List<String>>.filled(questions.length, []);
     });
   }
 
@@ -124,8 +126,8 @@ class DhwaniApp_QuestionnairePageState extends State<DhwaniApp_QuestionnairePage
             question: questions[index],
             onAnswerSelected: (List<String> selectedOptions) {
               debugPrint(selectedOptions.toString());
-              selectedAnswers[index] = selectedOptions;
-              selectedAnswersMap[questions[index].questionText] = selectedOptions.isNotEmpty ? selectedOptions[0] : '';
+              selectedFavourites[index] = selectedOptions;
+              selectedFavouritesMap[questions[index].questionText] = selectedOptions.isNotEmpty ? selectedOptions[0] : '';
             },
           );
         },
@@ -133,8 +135,9 @@ class DhwaniApp_QuestionnairePageState extends State<DhwaniApp_QuestionnairePage
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           // Load card data to Hive and set isFav based on selected answers
-          // print('Selected Answers Map: $selectedAnswersMap'); // debug
-          await _loadCardDataToHiveAndUpdate();
+          // print('Selected Answers Map: $selectedFavouritesMap'); // debug
+
+          // await _loadCardDataToHiveAndUpdate();
 
           // Print contents of cardBox
           // for (var i = 0; i < cardBox.length; i++) {
@@ -143,7 +146,7 @@ class DhwaniApp_QuestionnairePageState extends State<DhwaniApp_QuestionnairePage
           // }
 
           // Redirect to Home_Page
-          Navigator.push(context, MaterialPageRoute(builder: (context) => const DhwaniApp_QuestionnairePage2()));
+          Navigator.push(context, MaterialPageRoute(builder: (context) => DhwaniApp_QuestionnairePage2(selectedFavourites: selectedFavourites,)));
         },
         child: const Icon(Icons.check),
       ),
