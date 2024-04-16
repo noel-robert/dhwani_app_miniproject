04104a2c83d5e44fd2522b08f8035f830536f904
Noel John Robert <noel060202@gmail.com>
2024-04-16 20:47:11 +0530

    added extra files in preparation for using .env files for storing api keys

---
+++ lib/env/env.dart
@@ -0,0 +1,11 @@
+// import 'package:envied/envied.dart';
+// part 'env.g.dart';
+//
+// @Envied(path: '.env')
+// abstract class Env {
+//   @EnviedField(varName: 'TAG_GENERATOR_KEY', obfuscate: true)
+//   static const String tagGeneratorKey = _Env.tagGeneratorKey;
+//
+//   @EnviedField(varName: 'NWP_BERT_KEY', obfuscate: true)
+//   static const String nwpBertKey = _Env.nwpBertKey;
+// }
